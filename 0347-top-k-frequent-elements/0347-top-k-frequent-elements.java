b522e2d7a1cda2fd9c7ae2fa504473ee685a442e
Babisha1511 <babisha1511@gmail.com>
2026-06-16 23:39:03 +0530

    Time: 16 ms (40.66%), Space: 47.6 MB (47.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,13 +4,18 @@ class Solution {
        for(int i=0;i<nums.length;i++){
         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
        }
-       List<Map.Entry<Integer,Integer>> list=new ArrayList<>(map.entrySet());
-       list.sort((a,b)->b.getValue().compareTo(a.getValue()));
+       ArrayList<Map.Entry<Integer,Integer>> list=new ArrayList<>(map.entrySet());
+       Collections.sort(list,(a,b)->{
+        if(b.getValue().equals(a.getValue())){
+            return b.getKey()-a.getKey();
+        }
+        return b.getValue()-a.getValue();
+       });
        int[] ans=new int[k];
-       int i=0;
-       for(Map.Entry<Integer,Integer> m:list){
+       int j=0;
+       for(Map.Entry<Integer,Integer> entry:list){
         if(k-->0){
-          ans[i++]=m.getKey();
+          ans[j++]=entry.getKey();
         }
        }
        return ans;
